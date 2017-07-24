77898f90a24aa858da38a632f783a0edef0c0254
helay11 <vvc695637780@126.com>
2017-07-24 17:23:26 +0800

    first commit

---
+++ test_opencv.cpp
@@ -0,0 +1,25 @@
+#include<cv.h>
+#include<highgui.h>
+#include<iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    Mat image;
+    image = imread(argv[1], 1);
+    
+    if(argc != 2 || !image.data)
+    {
+	cout << "No image data "<< endl;
+	return -1;
+	
+    }
+    
+    namedWindow("Display Image", CV_WINDOW_AUTOSIZE);
+    imshow("Display Image", image);
+    waitKey(0);
+    return 0;
+}
+
